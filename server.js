2afa3b3e5cb65ab851170f0d2080c5792b5f23b6
john.leaderg.com <john@leaderg.com>
2015-05-04 23:32:38 +0800

    Change port to match environment

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var http = require("http");
 var url = require("url");
 
+var express = require("express");
+//var app = express();
+var port = process.env.PORT || 5000;
+
 function start(route, handle) {
   function onRequest(request, response) {
+  //app.get('/', function(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     route(handle, pathname, response, request);
-  }
+  };
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  http.createServer(onRequest).listen(port);
+  console.log(" Server has started on port: " + port);
 }
 
 exports.start = start;
